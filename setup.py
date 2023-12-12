67da6cddd73bac098634cb9f738f243f05e20239
Marco <marco.calandro@mensa.it>
2023-12-12 19:34:42 +0100

    Prepare for release 0.1.0
    - Added link to source and docs for PyPI
    - Rebuilt docs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf8") as f:
+with open("README.rst", "r", encoding="utf8") as f:
     readme = f.read()
 
 setup(
@@ -13,13 +13,17 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/MCallagher/pathtreelib",
+    project_urls={
+        "Documentation": "https://pathtreelib.readthedocs.io/en/latest/",
+        "source": "https://github.com/MCallagher/pathtreelib"
+    },
     author="Marco Calandro",
     author_email="marco.calandro@mensa.it",
     license="MIT",
     packages=["pathtreelib"],
     install_requires=["xlsxwriter>=3.1.9"],
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Topic :: Software Development :: Libraries",
         "Topic :: System :: Filesystems",
         "Topic :: Scientific/Engineering :: Information Analysis",
@@ -31,5 +35,5 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13"
-    ],
+    ]
 )
