c8434cc4dd9d20cdf8da2f3dc86783e6e7c038c8
Marco <marco.calandro@mensa.it>
2023-12-27 21:40:56 +0100

    Add callable main for analytcs module
    - Added callable main for analytics module

--- pathtreelib/analytics.py
+++ pathtreelib/analytics.py
@@ -1,6 +1,8 @@
 """ Set of analytics tools for PathTree.
 """
 
+import argparse
+
 from pathtreelib import PathNode, PathTree, PathTreeProperty
 
 
@@ -59,3 +61,47 @@ class PathTreeAnalytics(PathTree):
                 large_nodes.append(node)
                 queue += node.children
         return large_nodes
+
+
+def main():
+    """ Run a basic test on the pwd.
+    The results of the test are printed and can be exported in csv and Excel.
+    """
+
+    # Parse command line parameters
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-k", "--k",     type=int, default="10", 
+        help="The number of large Path to examine"
+    )
+    parser.add_argument(
+        "-x", "--excel", type=str, default="",
+        help="The Excel file for export (only if specified)"
+    )
+    parser.add_argument(
+        "-c", "--csv",   type=str, default="",
+        help="The csv file for export (only if specified)"
+    )
+    params = parser.parse_args()
+
+    # Create tree and perform analysis
+    tree = PathTreeAnalytics(".")
+    largest = tree.get_k_largest_nodes(params.k)
+
+    # Pretty print results
+    max_len = max(list(len(node.path.as_posix()) for node in largest))
+    for node in largest:
+        print(
+            f"{node.path.as_posix().ljust(max_len)} > " +
+            f"{node.property[PathTreeProperty.SIMPLE_SIZE]:>6s}"
+        )
+
+    # Export results
+    if params.excel != "":
+        tree.to_excel(params.excel, node_condition=lambda node: node in largest)
+    if params.csv != "":
+        tree.to_csv(params.csv, node_condition=lambda node: node in largest)
+
+
+if __name__ == "__main__":
+    main()
